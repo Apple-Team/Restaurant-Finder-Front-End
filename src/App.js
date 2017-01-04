29e1ad78a46cabd5356f22d263064f450bd777d3
ManishaBodapunti <bodapuntimanisha@gmail.com>
2017-01-05 01:03:33 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -8,26 +8,10 @@ import IconText from './IconText';
 
 class App extends Component {
   constructor() {
-    // In a constructor, call `super` first if the class extends another class
+    // In a constructor, call `super` first if the className extends another className
     super();
-
-    // Initialize state in the constructor; this is the only place you
-    // can set the state directly without using `this.setState`
-    this.state = { text: '' };
-
-    // This binding is necessary to make `this` work in the callback
-    this.updateText = this.updateText.bind(this);
   }
-
-  // Keep the state in sync with the textarea's value
-  updateText(ev) {
-    // Access the textarea's value just like you would with the brower's native events
-    const value = ev.currentTarget.value;
-
-    // WRONG: this.state.text = value;
-    this.setState({ text: value });
-  }
-
+   
   // `render` is called whenever the component's props OR state are updated.
   render() {
     // console.log('The App component was rendered');
@@ -35,7 +19,7 @@ class App extends Component {
     return (
 
     <div className="App">
-
+       
       <div className="App1">
 
         <div className="container">
@@ -45,25 +29,69 @@ class App extends Component {
               <li><a href="#">SignUp</a></li>
               <li><a href="#">Login</a></li>
           </ul>
+        </div>
+        <div className="wrapper">
+          <div className="container">
+           <br /><br /><br  /><br /><br /><br  /><br /><br /><br />
 
 
-      </div>
-      <div className="wrapper">
-           <div className="container">
-           <br /><br /><br  /><br /><br /><br  /><br /><br /><br  /><br /><br /><br  />
-
-
-           <div className="form-horizontal">
-            <input type="text" name="..." className="form-control" placeholder="Hungry??  Find your favourite /bistro........"/>
+            <div className="form-horizontal">
+            <input type="text" name="..." className="form-control" placeholder="Hungry??  Find your favourite Bistro........"/>
               <button type="submit" className="btn btn-primary" >
                 <i className="glyphicon glyphicon-search"></i>
                 </button>
-                </div>
+            </div>
+          </div>
+<br /><br /><br  />
 
-       </div>
+ <div className="row text-center">
+  <div className="col-sm-4">
+    <div className="img-responsive">
+      <a href="#"><img src={require('./images/sb.jpg')} alt="Paris" /></a>
+    </div>
+    <p><i>Sunday Brunch</i></p>
+  </div>
+  <div className="col-sm-4">
+    <div className="img-responsive">
+      <a href="#"><img src={require('./images/fd.jpg')} alt="New York" /></a>
+    </div>
+    <p><i>Frozen Delight</i></p>
+    </div>
+  <div className="col-sm-4">
+    <div className="img-responsive">
+     <a href="#"><img src={require('./images/street.jpg')} alt="San Francisco" /></a>
+    </div>
+    <p><i>Street Food</i></p>
+  </div>
+</div>
+<br /><br /><br />
+<div className="row text-center">
+  <div className="col-sm-4">
+    <div className="img-responsive">
+      <a href="#"><img src={require('./images/dinner.jpeg')} alt="Paris" /></a>
+    </div>
+    <p><i>Fine Dine</i></p>
+  </div>
+  <div className="col-sm-4">
+    <div className="img-responsive">
+      <a href="#"><img src={require('./images/buffet.jpg')} alt="New York" /></a>
+    </div>
+    <p><i>Buffet</i></p>
+  </div>
+  <div className="col-sm-4">
+    <div className="img-responsive">
+      <a href="#"><img src={require('./images/dessert.jpg')} alt="San Francisco" /></a>
+    </div>
+    <p><i>Desserts</i></p>
+  </div>
+</div>
+
+
+
      </div>
     </div>
-  </div>
+</div>
+
 
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import 'isomorphic-fetch';
 
 import App from './App';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'font-awesome/css/font-awesome.css';
+
+import './bootstrap-dist/css/bootstrap.css';
+import './font-awesome/css/font-awesome.css';
 import './index.css';
 
 ReactDOM.render(
+
   <App />,
   document.getElementById('root')
 );
